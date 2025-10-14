d281dbe5641e97c636e72d974279eafca7e990b5
Muhammad Ismail <112934517+ismailfaridi@users.noreply.github.com>
2025-10-14 20:02:29 +0500

    Chat App Project Setup

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router;
