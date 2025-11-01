cfb6bc1665bb61069a767698f8557e5bf133c6c0
Muhammad Ismail <112934517+ismailfaridi@users.noreply.github.com>
2025-11-01 10:27:04 +0500

    Message UI completed

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from 'react';
 import { useChatStore } from '../store/useChatStore.js';
+import { useAuthStore } from '../store/useAuthStore.js';
 import ChatHeader from './ChatHeader.jsx';
 import MessageInput from './MessageInput.jsx';
 import MessageSkeleton from "./skeletons/MessageSkeleton.jsx";
+import { formatMessageTime } from '../lib/utils.js';
 
 const ChatContainer = () => {
   const { messages, isMessagesLoading, selectedUser, getMessages } = useChatStore();
+  const { authUser } = useAuthStore();
 
   useEffect(() => {
     getMessages(selectedUser._id);
@@ -25,7 +28,31 @@ const ChatContainer = () => {
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
 
-      <p>messages...</p>
+      <div className='flex-1 overflow-y-auto p-4 space-y-4'>
+        {messages.map((message) => (
+          <div key={message._id}
+          className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
+            {/* chat profile avatar */}
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img src={message.senderId === authUser._id ? authUser.profilePic || "/avatar.png" :
+                selectedUser.profilePic || "/avatar.png"} alt="profile pic" />
+              </div>
+            </div>
+            {/* timestamp */}
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">{formatMessageTime(message.createdAt)}</time>
+            </div>
+            {/* chat: image, text */}
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img src={message.image} alt="Attachment" className="sm:max-w-[200px] rounded-md mb-2" />
+              )}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
 
       <MessageInput />
     </div>
