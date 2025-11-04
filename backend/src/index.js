e833ac50468168bf70f48d79e48cad284a4d39c9
Muhammad Ismail <112934517+ismailfaridi@users.noreply.github.com>
2025-11-04 08:39:20 +0500

    typo

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     // Other routes then above routes middleware
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
 }
